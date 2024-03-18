c6196dddd98297d636070737f358856244b6d144
amanikitutocd.. <akituto@gmail.com>
2024-03-18 13:25:20 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// Implementing a function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Implementing a function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Implementing a function returning a function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  // Test the functions
+  console.log(saturdayFun()); // Output: "This Saturday, I want to roller-skate!"
+  console.log(mondayWork()); // Output: "This Monday, I will go to the office."
+  console.log(wrapAdjective("%")("a dedicated programmer")); // Output: "You are %a dedicated programmer%!"
+  
